70fdfb201201185ef88460ad6d4f1bbf1ce7c54a
Ezel <101886487+EzelPijn@users.noreply.github.com>
2022-05-22 14:48:38 -0400

    Working JS

--- 01 - JavaScript Drum Kit/JS/index.js
+++ 01 - JavaScript Drum Kit/JS/index.js
@@ -25,59 +25,105 @@ On keydown:
     remove class .playing
 
 
-Need to make one function to play the sound, which will be switch statement
+Need to make one function to play the sound, which will be switch statement - DONE
 
-Need to make one function to change the class
+Need to make one function to change the class 
+    - on click
+    - on keydown - DONE
 
-Feed the above two functions into two functions - one on keydown, one on click
+Feed the above two functions into two functions - one on keydown - DONE, one on click
 
 */
+
+//Does not work to identify and capture the key id of the key pressed
+/*
+$(this).keydown (function () {
+    var currentPressedKey = $(this).attr("id");
+    console.log(currentPressedKey);
+});*/
 
 
 
 //Identifies and Captures the key id of the key clicked
 $(".key").click(function () {
     var currentKey = $(this).attr("id");
+    $("#" + currentKey).addClass("playing");
+    setTimeout(function () {
+        $("#" + currentKey).removeClass("playing");
+    }, 100)
     console.log(currentKey);
+    generateSound(currentKey);
 })
 
-//Does not work to identify and capture the key id of the key pressed
-$(this).keydown(function () {
-    var currentKey = $(this).attr("id");
-    console.log(currentKey);
-})
 
+document.addEventListener ("keydown", function(event) {
+    generateSound(event.key);
+    buttonAnimation(event.key);
+    console.log(event.key);
+});
 
-
-//Plays Sound on click of specific box//
-$('[data-key="65"]').click(function (){
-    var clapAudio = new Audio("sounds/clap.wav");
-    clapAudio.play();
-    $('[data-key="65"]').addClass("playing");
-    setTimeout(function () {
-        $('[data-key="65"]').removeClass("playing");
-    }, 100)
-} )
-
-//Plays Sound on keydown of any key//
-$(this).keydown(function (){
-    var clapAudio = new Audio("sounds/clap.wav");
-    clapAudio.play();
-    $('[data-key="65"]').addClass("playing");
-    setTimeout(function () {
-        $('[data-key="65"]').removeClass("playing");
-    }, 100)
-} )
-
-/*
-/function generateSound() {
-    switch (key) {
-        case value:
-            
-            break;
+//Adds and Removes Playing Class on Keydown//
+function buttonAnimation(key){
+    var activeButton = $("." + key);
     
+    activeButton.addClass("playing");
+    setTimeout(function() {
+        activeButton.removeClass("playing");
+    }, 100);
+}
+
+
+function generateSound(key) {
+    switch (key) {
+        case "65":
+        case "a":
+            var clap = new Audio ("sounds/clap.wav");
+            clap.play();
+            break;   
+        case "83":
+        case "s":
+            var hihat = new Audio ("sounds/hihat.wav");
+            hihat.play();
+            break;
+        case "68":
+        case "d":
+            var kick = new Audio ("sounds/kick.wav");
+            kick.play();
+            break;
+        case "70":
+        case "f":
+            var openhat = new Audio ("sounds/openhat.wav");
+            openhat.play();
+            break;
+        case "71":
+        case "g":
+            var boom = new Audio ("sounds/boom.wav");
+            boom.play();
+            break;
+        case "72":
+        case "h":
+            var ride = new Audio ("sounds/ride.wav");
+            ride.play();
+            break;
+        case "74":
+        case "j":
+            var snare = new Audio ("sounds/snare.wav");
+            snare.play();
+            break;
+        case "75":
+        case "k":
+            var tom = new Audio ("sounds/tom.wav");
+            tom.play();
+            break;
+        case "76":
+        case "l":
+            var tink = new Audio ("sounds/tink.wav");
+            tink.play();
+            break;
+        
         default:
             break;
     }
-}*/
+}
+
 
