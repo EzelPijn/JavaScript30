f960cc5299e8a44fdb342b7aec49d1ee70573681
Ezel <101886487+EzelPijn@users.noreply.github.com>
2022-05-26 11:29:37 -0400

    Final HTML CSS JS

---
+++ 02 - JS and CSS Clock/JS/index.js
@@ -0,0 +1,28 @@
+const secondHand = $(".second-hand");
+const minuteHand = $(".min-hand");
+const hourHand = $(".hour-hand")
+
+
+function setDate(){
+    const today = new Date();
+
+    const seconds = today.getSeconds();
+    const secondsDegrees = ((seconds/60)*360)+90;
+    secondHand.css("transform", `rotate(${secondsDegrees}deg)`);
+    /*secondHand.style.transform = `rotate(${secondsDegrees}deg)`;*/
+
+
+    const minutes = today.getMinutes();
+    const minutesDegrees = ((minutes/60)*360)+90;
+    minuteHand.css("transform", `rotate(${minutesDegrees}deg)`);
+
+
+    const hours = today.getHours();
+    const hoursDegrees = ((hours/12)*360)+90;
+    hourHand.css("transform", `rotate(${hoursDegrees}deg)`);
+
+    console.log(minutes);
+}
+
+setInterval(setDate, 1000);
+
