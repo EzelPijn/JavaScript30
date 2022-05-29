f4b37c75f1f2365e9e42f07f5d186765d3cbb774
Ezel <101886487+EzelPijn@users.noreply.github.com>
2022-05-29 13:21:22 -0400

    Final HTML CSS JS

--- 03 - CSS Variables/JS/index.js
+++ 03 - CSS Variables/JS/index.js
@@ -1,19 +1,3 @@
-/* 
-To blur: filter: blur(8px);
-Spacing: border: 20px solid #ff9000;
-Base: color picker to feed the border in spacing
-
-
-Create function that will add border to image based on the slider of the #spacing
-
-Create function that will blur the image based on the slider of #blur
-
-Create function that will change the border color of the image based on the color selected in #base
-
-
-*/
-
-
 var spacing = document.getElementById("spacing");
 var blur = document.getElementById("blur");
 var base = document.getElementById("base");
@@ -29,5 +13,11 @@ function adjustSpacing() {
 function adjustBlur() {
     var blurValue = blur.value;
     document.getElementById("image").style.filter = `blur(${blurValue}px)`;
+}
+
+function adjustBorderColor() {
+    var baseValue = base.value;
+    document.getElementById("image").style.borderColor = `${baseValue}`;
+    document.getElementById("JS").style.color = `${baseValue}`;
 }
 
